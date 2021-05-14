8e51b7552818e64285dabd6ce641ab4568776df7
kappleword <kappleword@gmail.com>
2021-05-14 11:37:30 +0900

    생성자 객체주입법 코드 예제2

--- spring4_1_1/src/local/di/Sonata.java
+++ spring4_1_1/src/local/di/Sonata.java
@@ -6,7 +6,7 @@ package local.di;
 public class Sonata {
 	private String carColor = null;
 	private int wheelNum = 0;
-	private int speeed = 0;
+	private int speed = 0;
 	public Sonata() {}
 	public Sonata(String carColor, int wheelNum) {
 		this.carColor = carColor;
@@ -15,11 +15,29 @@ public class Sonata {
 	public Sonata(String carColor, int wheelNum, int speed) {
 		this.carColor = carColor;
 		this.wheelNum = wheelNum;
-		this.speeed = speed;
+		this.speed = speed;
 	}
 	@Override
 	public String toString() {
 		return "자동차정보출력하기";
 	}
 
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getWheelNum() {
+		return wheelNum;
+	}
+	public void setWheelNum(int wheelNum) {
+		this.wheelNum = wheelNum;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
 }
